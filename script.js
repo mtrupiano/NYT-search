cb7cb59682c01b9cf5bc88c74b94f7d6d85affb3
mtrupiano <markt6749@gmail.com>
2020-12-18 10:52:37 -0800

    more drafting; fixed jQuery link

--- script.js
+++ script.js
@@ -2,20 +2,38 @@
 
 $(document).ready(function() {
 
-    $("#search-btn").on("click", function(event) {
+    $("#search-button").click(function(event) {
         event.preventDefault();
 
         var query       = $("#search-term-input").val();
         var beginYear   = $("#start-year-input").val();
         var endYear     = $("#end-year-input").val();
 
-        var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="
-            + query + "&begin_date=" + beginYear + "0101&end_date=" + endYear + 
-            "0101&api-key=u7EoghAalikiEAE7MRKG4oMkPIm9RhOE";
+        if (beginYear !== "") {
+            beginYear = "&begin_date=" + beginYear + "0101";
+        }
+        
+        if (endYear !== "") {
+            beginYear = "&end_date=" + endYear + "0101";
+        }
 
+        var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="
+            + query + beginYear + endYear + 
+            "&api-key=u7EoghAalikiEAE7MRKG4oMkPIm9RhOE";
+    
         $.ajax({url: queryURL, method: "GET"}).then(function(response) {
+            renderArticles(response.docs);
             console.log(response);
         });
+
     });
+
+    $("#clear-button").click(function(event) {
+
+    });
+
+    function renderArticles(articles) {
+        
+    }
 
 });
