76362ce13c6c38034275575792eb3ee8e3f21afa
Nolan Stucky <nolan@stucky.net>
2020-12-18 10:53:33 -0800

    starting to add article section

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 
 
 $(document).ready(function() {
-
+   
     $("#search-btn").on("click", function(event) {
         event.preventDefault();
 
         var query       = $("#search-term-input").val();
         var beginYear   = $("#start-year-input").val();
         var endYear     = $("#end-year-input").val();
+
 
         var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="
             + query + "&begin_date=" + beginYear + "0101&end_date=" + endYear + 
@@ -17,5 +18,9 @@ $(document).ready(function() {
             console.log(response);
         });
     });
+
+    $("clear-results-button").on("click",function(event){
+        
+    })
 
 });
