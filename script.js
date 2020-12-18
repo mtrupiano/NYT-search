3ac0c9cb1122c9a81a262621674872bc88361381
mtrupiano <markt6749@gmail.com>
2020-12-18 10:26:15 -0800

    drafting

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 
 
 $(document).ready(function() {
-    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + query + "&api-key=u7EoghAalikiEAE7MRKG4oMkPIm9RhOE"
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" 
+                    + query + "&api-key=u7EoghAalikiEAE7MRKG4oMkPIm9RhOE"
 
 
     var query;
     
-    $("").on("click", function(event) {
+    var searchBtn;
+    var clearBtn;
+    var beginYearField;
+    var endYearField;
+    var numberField;
+    var resultsContainer;
+
+    $("#search-btn").on("click", function(event) {
 
         query = $("#term-field").text();
+        $.ajax({url: queryURL, method: "GET"}).then(function(response) {
+            console.log(response);
+        });
     });
+
+    function renderResults() {
+
+    }
 });
